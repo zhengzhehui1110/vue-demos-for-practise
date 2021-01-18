76d323d45ed038cc22d255ef4f472fbf80535b88
zhengzhehui <zzh@szltech.com>
2021-01-18 16:56:43 +0800

    1.18

--- src/views/AniGroup.vue
+++ src/views/AniGroup.vue
@@ -4,7 +4,7 @@
         <el-row :gutter="10">
             <el-col :span="12">
                 <h4>排序过渡</h4>
-                <transition-group name="flip-list" tag="ul">
+                <transition-group name="flip-list">
                     <li v-for="item in todoList" v-bind:key="item">
                         {{item}}
                     </li>
@@ -14,6 +14,31 @@
                 <el-button type="primary" 
                 icon="el-icon-check" circle
                 @click="shuffle"></el-button>
+            </el-col>
+        </el-row>
+
+        <el-row :gutter="10">
+            <el-col :span="12">
+                <h4>列表进入/退出</h4>
+                <transition-group name="list" tag="ul">
+                    <li class="list-item"
+                    v-for="item in subject" v-bind:key="item">
+                        {{item}}
+                    </li>
+                </transition-group>
+            </el-col>
+            <el-col :span="6">
+                <el-input v-model="newSubject" placeholder="请输入新增项"></el-input>
+            </el-col>
+            <el-col :span="3">
+                <el-button type="primary" 
+                icon="el-icon-plus" circle
+                @click="add"></el-button>
+            </el-col>
+            <el-col :span="3">
+                <el-button type="primary" 
+                icon="el-icon-minus" circle
+                @click="remove"></el-button>
             </el-col>
         </el-row>
     </div>
@@ -30,13 +55,30 @@ export default {
                 'make a phone call',
                 'do my homework',
             ],
+            subject:[
+                'Math',
+                'English',
+                'Data Science',
+                'Deep Learning',
+                'Software Engeneer'
+            ],
+            newSubject:'',
         }
     },
     methods: {
-    shuffle: function () {
-        // 这里引入了lodash.js库
-      this.todoList = this._.shuffle(this.todoList)
-    }
+        randomIndex: function () {
+            return Math.floor(Math.random() * this.subject.length)
+        },
+        shuffle: function () {
+            // 这里引入了lodash.js库
+            this.todoList = this._.shuffle(this.todoList)
+        },
+        remove: function () {
+            this.subject.splice(this.randomIndex(), 1)
+        },
+        add: function () {
+            this.subject.splice(this.randomIndex(), 0, this.newSubject)
+        },
   },
 
 }
@@ -45,5 +87,29 @@ export default {
 <style lang="css">
     .flip-list-move {
         transition: transform 1s;
+    }
+
+    .list-item {
+        transition: all 1s;
+    }
+    .list-enter-active, .list-leave-active {
+        transition: all 1s;
+    }
+    .list-enter, .list-leave-to
+        /* .list-leave-active for below version 2.1.8 */ {
+        opacity: 0;
+        transform: translateY(30px);
+        
+    }
+    /* .list-leave{
+        position: relative;
+        left: 0px;
+    } */
+    .list-leave-active {
+        position: absolute;
+        /*元素进入页面占据位置的时机是enter,
+        离开页面让出位置的时机是leave-to,
+        改变位置的动画只能在active时机进行
+         */
     }
 </style>
